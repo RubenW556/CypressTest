a2728e3d076b38515ede6345bc99e41810a452a4
ruben.wijnbergen <ruben.wijnbergen@novadoc.nl>
2024-03-18 14:45:37 +0100

    eh

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 describe('e2e tests', () => {
     it('should log-in', () => {
         cy.visit('http://win-1ics5jffm16.dev.local:9080/navigator/')
-        cy.contains('Gmail').click()
+        cy.contains('Welcome to IBM Content Navigator')
     })
 })
