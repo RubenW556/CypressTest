663d88714807b2184157d49a3926c00f628998f6
ruben.wijnbergen <ruben.wijnbergen@novadoc.nl>
2024-03-21 15:22:00 +0100

    eh

--- test.js
+++ test.js
@@ -3,23 +3,23 @@ describe('e2e tests', () => {
         cy.visit('http://win-1ics5jffm16.dev.local:9080/navigator/')
         cy.contains('Welcome to IBM Content Navigator')
     })
-    it('should log-in', () => {
+    it('should log-in1', () => {
         cy.visit('http://win-1ics5jffm16.dev.local:9080/navigator/')
         cy.contains('Welcome to IBM Content Navigator')
     })
-    it('should log-in', () => {
+    it('should log-in2', () => {
         cy.visit('http://win-1ics5jffm16.dev.local:9080/navigator/')
         cy.contains('Welcome to IBM Content Navigator')
     })
-    it('should log-in', () => {
+    it('should log-in3', () => {
         cy.visit('http://win-1ics5jffm16.dev.local:9080/navigator/')
         cy.contains('Welcome to IBM Content Navigator')
     })
-    it('should log-in', () => {
+    it('should log-in4', () => {
         cy.visit('http://win-1ics5jffm16.dev.local:9080/navigator/')
         cy.contains('Welcome to IBM Content Navigator')
     })
-    it('should log-in', () => {
+    it('should log-in5', () => {
         cy.visit('http://win-1ics5jffm16.dev.local:9080/navigator/')
         cy.contains('Welcome to IBM Content Navigator')
     })
